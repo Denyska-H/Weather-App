49c1f7f9d75601d053173ba6785f1c9ee44922c6
Denys Hleba <denis133445500@gmail.com>
2022-06-10 16:17:36 +0300

    New path to img folder

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -3,7 +3,7 @@ import fileInclude from "gulp-file-include";
 export const html = () => {
 	return app.gulp.src(app.path.src.html)
 		.pipe(fileInclude())
-		.pipe(app.plugins.replace(/@img\//g, "img/"))
+		.pipe(app.plugins.replace(/@img\//g, "dist/img/"))
 		.pipe(app.gulp.dest(app.path.build.html))
 		.pipe(app.plugins.browserSync.stream())
 }
